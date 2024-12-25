b056d33690eda1eb90a636eab0f164e7e870f1b8
spallav23 <145319192+spallav23@users.noreply.github.com>
2024-12-26 00:39:49 +0530

    Update Fdata.js

--- src/components/Fdata.js
+++ src/components/Fdata.js
@@ -2,7 +2,7 @@
 
 
 const Fdata = async() => {
-   const res = await fetch('http://universities.hipolabs.com/search?country=United+States')
+   const res = await fetch('https://api.jsonbin.io/v3/b/676c5741e41b4d34e46b17a0')
     return res.json();
 
 }
