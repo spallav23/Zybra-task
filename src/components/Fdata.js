96a0c481742a0208bece0f8d19bcaea9cb628d35
spallav23 <145319192+spallav23@users.noreply.github.com>
2024-12-26 00:51:02 +0530

    Update Fdata.js

--- src/components/Fdata.js
+++ src/components/Fdata.js
@@ -3,7 +3,7 @@
 
 const Fdata = async() => {
    const res = await fetch('https://api.jsonbin.io/v3/b/676c5741e41b4d34e46b17a0')
-   data = await res.json();
+   const data = await res.json();
    console.log(data);
     return data;
 
